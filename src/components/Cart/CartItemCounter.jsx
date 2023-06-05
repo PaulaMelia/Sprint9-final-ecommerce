c1642fbe7253605e421828b310e43900cd6dbddf
PaulaMelia <progne.liber@gmail.com>
2023-06-05 21:07:55 +0200

    css counter bottoms

--- src/components/Cart/CartItemCounter.jsx
+++ src/components/Cart/CartItemCounter.jsx
@@ -2,21 +2,35 @@ import PropTypes from "prop-types";
 import { useContext } from "react";
 import { dataContext } from "../DataContext";
 
+const CartItemCounter = ({ product, quanty }) => {
+  const { cart, setCart, buyProduct } = useContext(dataContext);
 
-const CartItemCounter = ({ product, quanty}) => {
-  const { buyProduct } = useContext(dataContext);
+  const decrese = () => {
+    const productRepeat = cart.find((item) => item.id === product.id);
+
+    productRepeat &&
+      productRepeat.quanty !== 1 &&
+      setCart((prevCart) =>
+        prevCart.map((item) =>
+          item.id === product.id ? { ...item, quanty: item.quanty - 1 } : item
+        )
+      );
+  };
+
   return (
-    <>
-      <p className="counter-button">-</p>
+    <div className="cart-item-counter">
+      <p className="counter-button" onClick={decrese}>
+        -
+      </p>
       <p>{quanty}</p>
       <p className="counter-button" onClick={() => buyProduct(product)}>
         +
       </p>
-    </>
+      </div>
   );
 };
 CartItemCounter.propTypes = {
-    quanty: PropTypes.number.isRequired,
-  };
+  quanty: PropTypes.number.isRequired,
+};
 
 export default CartItemCounter;
