f6de1943825b721b71933dbdfb79d31b1a434294
PaulaMelia <progne.liber@gmail.com>
2023-06-06 22:26:01 +0200

    ok navegacion

--- src/components/Cart/CartContent.jsx
+++ src/components/Cart/CartContent.jsx
@@ -1,5 +1,6 @@
 import CartElements from "./CartElements";
 import CartTotal from "./CartTotal";
+import Navbar from "../Navbar";
 import { useContext } from "react";
 import { dataContext } from "../DataContext";
 import "../../styles/CartContent.css";
@@ -8,14 +9,21 @@ const CartContent = () => {
   const { cart } = useContext(dataContext);
 
   /*Condition cart 0*/
-  return cart.length > 0 ? (
+  return (
     <>
+    <Navbar/>
+    {cart.length > 0 ? (
+      <>
       <CartTotal />
       <CartElements />
+      </>
+    ) :(
+      <h2 className="cart-message-center">Tu carito esta vacio</h2>
+  
+    )}
     </>
-  ) : (
-    <h2 className="cart-message-center">Tu carito esta vacio</h2>
-  );
-};
+  )};
+
+
 
 export default CartContent;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { faBicycle } from "@fortawesome/free-solid-svg-icons";
-import "../styles/Navbar.css";
+import { useContext } from "react";
+import { dataContext } from "../components/DataContext";
 import { Link } from "react-router-dom";
 import TotalItems from "./Cart/TotalItems";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const { cart } = useContext(dataContext);
   return (
     <div className="nav-container">
       <nav className="navbar">
+        <Link to={"/"} style={{ textDecoration: 'none', color: 'black' }}>
         <div className="navbar-logo-container">
           <div className="navbar-logo">
             <FontAwesomeIcon icon={faBicycle} className="bicycle-icon" />
             <span>BIKE SHOP</span>
           </div>
         </div>
+        </Link>
 
         <ul className="navbar-links">
           <li>
@@ -29,11 +33,11 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="navbar-cart">
-          <Link className="navbar-link" to={"/cart"}>
+          <Link className="navbar-link" to={"/cart"}style={{ textDecoration: 'none', color: 'black' }}>
             🛒
-          <TotalItems/>
+            {cart.length > 0 ? <TotalItems /> : null}
+            
           </Link>
-          
         </div>
       </nav>
     </div>
